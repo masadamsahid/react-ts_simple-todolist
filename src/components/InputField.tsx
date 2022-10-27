8ab4102b9b29778ec88d87308d915955bcb47955
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-10-27 22:32:03 +0700

    adding feature: input new to do

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import './style.css';
 
-const InputField = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+  handleAdd: (e:React.FormEvent) => void;
+}
+
+const InputField:React.FC<Props> = ({todo, setTodo, handleAdd}) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
       <input
         type='text'
         placeholder='Enter a task'
         className='input__box'
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
       />
       <button className="input_submit" type='submit'>
         Go
